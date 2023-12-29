177881a0048e4147459046d015bd40f3b499c44b
Zvi "CtrlZvi" Effron <viz_skywalker+GitHub@outlook.com>
2023-12-28 17:32:41 -0800

    Perform automatic migrations in development
    When database schemas change in development environments, it is
    irritating to have to manually perform a database migration or restart
    the server to have it apply the necessary migrations. The new database
    access library uses an internal wrapper function that detects failures
    because a database migration is necessary and runs available
    migrations. While migrations still need to be generated manually, they
    will be applied automatically when rendering the first page that needs
    them.

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,11 +1,9 @@
-
 export async function register() {
     if (process.env.NEXT_RUNTIME === 'nodejs') {
         console.info("Migrating database...");
         await import('fs');
-        const { migrate } = await import('drizzle-orm/better-sqlite3/migrator');
-        const { db } = await import('@/migration');
-        migrate(db, { migrationsFolder: '.drizzle' });
+        const { performMigrations } = (await import('@/lib/database'));
+        performMigrations();
         console.info("Migration complete...");
     }
 }
---
+++ src/lib/database.ts
@@ -0,0 +1,50 @@
+import Database, { SqliteError } from 'better-sqlite3';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+
+import * as accountSchema from '@/app/accounts/schema';
+
+const database = drizzle(new Database('zyf.db'), { schema: { ...accountSchema }, logger: true });
+
+export function performMigrations() {
+    migrate(database, { migrationsFolder: '.drizzle' });
+}
+
+function migrateIfNecessary<This, Args extends any[], Return>(original: (this: This, ...args: Args) => Return) {
+    const replacement = function (this: This, ...args: Args) {
+        try {
+            return original.apply(this, args);
+        } catch (error) {
+            if (process.env.NODE_ENV !== 'development') {
+                // We only automigrate in development as a convenience to the
+                // developer. In production, we expect there to be no changes
+                // since the migration on startup.
+                throw error;
+            }
+            console.log(`Error is a SqliteError? ${error instanceof SqliteError}`);
+            if (!(error instanceof SqliteError)) {
+                throw error;
+            }
+
+            if (error.message.startsWith("no such column: ")) {
+                console.warn("Missing column detected, running migrations...")
+            } else if (error.message.startsWith("no such table: ")) {
+                console.warn("Missing table detected, running migrations...")
+            } else {
+                throw error;
+            }
+            performMigrations();
+            console.info("Migration complete...");
+            return original.apply(this, args);
+        }
+    }
+    return replacement;
+}
+
+export const fetchAccounts = migrateIfNecessary(() => {
+    // TODO (zeffron 2023-12-28) Figure out how to perform the prepare once. We
+    // need to do the preparation inside this function as it could throw an
+    // error if the migrations haven't run, but we need to memoize the result
+    // (with type information).
+    return database.query.accounts.findMany().prepare().all();
+})
